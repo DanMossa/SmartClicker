c77910a1b85a814dd1ef8b135429f5bbf4fee6ef
Dgameman1 <danmossa@gmail.com>
2015-05-01 01:59:31 -0700

    Commit solution

---
+++ AutoClicker/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSeed = new System.Windows.Forms.Button();
+            this.labelSeed = new System.Windows.Forms.Label();
+            this.timerSeed = new System.Windows.Forms.Timer(this.components);
+            this.clicksPerSecond = new System.Windows.Forms.TextBox();
+            this.refine = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timerAutoClick = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSeed
+            // 
+            this.buttonSeed.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonSeed.Location = new System.Drawing.Point(136, 57);
+            this.buttonSeed.Name = "buttonSeed";
+            this.buttonSeed.Size = new System.Drawing.Size(166, 57);
+            this.buttonSeed.TabIndex = 1;
+            this.buttonSeed.Text = "Click this button as fast as you can for 30 seconds";
+            this.buttonSeed.UseVisualStyleBackColor = true;
+            this.buttonSeed.Click += new System.EventHandler(this.buttonClickFast_Click);
+            // 
+            // labelSeed
+            // 
+            this.labelSeed.AutoSize = true;
+            this.labelSeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSeed.Location = new System.Drawing.Point(201, 117);
+            this.labelSeed.Name = "labelSeed";
+            this.labelSeed.Size = new System.Drawing.Size(36, 25);
+            this.labelSeed.TabIndex = 2;
+            this.labelSeed.Text = "30";
+            // 
+            // timerSeed
+            // 
+            this.timerSeed.Interval = 1000;
+            this.timerSeed.Tick += new System.EventHandler(this.timerSeed_Tick);
+            // 
+            // clicksPerSecond
+            // 
+            this.clicksPerSecond.Location = new System.Drawing.Point(12, 93);
+            this.clicksPerSecond.Name = "clicksPerSecond";
+            this.clicksPerSecond.ReadOnly = true;
+            this.clicksPerSecond.Size = new System.Drawing.Size(100, 20);
+            this.clicksPerSecond.TabIndex = 3;
+            // 
+            // refine
+            // 
+            this.refine.Location = new System.Drawing.Point(320, 12);
+            this.refine.Name = "refine";
+            this.refine.Size = new System.Drawing.Size(106, 23);
+            this.refine.TabIndex = 4;
+            this.refine.Text = "Refine Autoclicker";
+            this.refine.UseVisualStyleBackColor = true;
+            this.refine.Click += new System.EventHandler(this.refine_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Move your mouse to center of where you want to click and press ` to start/stop";
+            // 
+            // timerAutoClick
+            // 
+            this.timerAutoClick.Tick += new System.EventHandler(this.timerAutoClick_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // webControl1
+            // 
+            this.webControl1.Location = new System.Drawing.Point(432, 12);
+            this.webControl1.Size = new System.Drawing.Size(497, 360);
+            this.webControl1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 443);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.refine);
+            this.Controls.Add(this.clicksPerSecond);
+            this.Controls.Add(this.labelSeed);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSeed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DG Smart AutoClicker 0.0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSeed;
+        private System.Windows.Forms.Label labelSeed;
+        private System.Windows.Forms.Timer timerSeed;
+        private System.Windows.Forms.TextBox clicksPerSecond;
+        private System.Windows.Forms.Button refine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerAutoClick;
+        private System.Windows.Forms.TextBox textBox1;
+        private Awesomium.Windows.Forms.WebControl webControl1;
+    }
+}
+
---
+++ AutoClicker/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace AutoClicker
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            webControl1.Source = new Uri("http://mossaband.com/AutoClicker/login.html");
+        GetTemp:
+
+            int retry = 0;
+            try
+            {
+                WebClient Weather = new WebClient();
+                string htmlWeather = Weather.DownloadString("http://api.openweathermap.org/data/2.5/weather?zip=90212,us&units=imperial");
+                MatchCollection temperature = Regex.Matches(htmlWeather, "\"temp\":(.{5})", RegexOptions.Singleline);
+
+                foreach (Match currentTemp in temperature)
+                {
+                    textBox1.Text = currentTemp.Value;
+                }
+                string temp = textBox1.Text;
+                textBox1.Text = temp.Remove(0, 7);
+            }
+            catch (Exception ex)
+            {
+                if (retry <= 3)
+                {
+                    goto GetTemp;
+                }
+                DialogResult res = MessageBox.Show("Can't get Account Information:\n" + ex.Message, "Error",
+                    MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (res == DialogResult.Retry)
+                {
+                    goto GetTemp;
+                }
+                else if (res == DialogResult.Cancel)
+                {
+                    MessageBox.Show("Please send an email to Daniel@Mossaband regarding the issue.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Application.Exit();
+                }
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            
+            int xPosition = Cursor.Position.X;
+            int yPosition = Cursor.Position.Y;
+            Console.Write(xPosition + " " + yPosition);
+
+        }
+
+        double clicks = 0;
+
+        private void buttonClickFast_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(labelSeed.Text) > 0 )
+            {
+                clicks = clicks + 1;
+            }
+            if (!timerSeed.Enabled)
+            {
+                timerSeed.Start();
+            }
+        }
+
+        private void timerSeed_Tick(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(labelSeed.Text) > 0)
+            {
+                labelSeed.Text = Convert.ToString(Convert.ToInt32(labelSeed.Text) - 1);
+            }
+            else if (Convert.ToInt32(labelSeed.Text) <= 0)
+            {
+                buttonSeed.Visible = false;
+                labelSeed.Visible = false;
+                timerSeed.Stop();
+                clicksPerSecond.Text = Convert.ToString(clicks / 30);
+            }
+        }
+
+        private void refine_Click(object sender, EventArgs e)
+        {
+            labelSeed.Text = "30";
+            buttonSeed.Visible = true;
+            labelSeed.Visible = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (buttonSeed.Visible == false)
+            {
+                if (e.KeyCode == Keys.Oemtilde)
+                {
+                    if (timerAutoClick.Enabled == false)
+                    {
+
+                        timerAutoClick.Start();
+                    }
+                    else if (timerAutoClick.Enabled == true)
+                    {
+                        timerAutoClick.Stop();
+                    }
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    int xPosition = Cursor.Position.X;
+                    int yPosition = Cursor.Position.Y;
+                   
+                    Console.Write(xPosition + " " + yPosition);
+                }
+            } 
+
+        }
+
+        private void timerAutoClick_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
