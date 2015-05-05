ad8dfe23d37b52fd12d5d366ef8ada78d2e186b7
Dgameman1 <danmossa@gmail.com>
2015-05-05 16:27:18 -0700

    Added comments

--- AutoClicker/Form1.cs
+++ AutoClicker/Form1.cs
@@ -18,6 +18,7 @@ namespace AutoClicker
 
     public partial class Form1 : Form
     {
+        //Code for changing webBrowser User Agent
         [DllImport("urlmon.dll", CharSet = CharSet.Ansi)]
         private static extern int UrlMkSetSessionOption(int dwOption, string pBuffer, int dwBufferLength, int dwReserved);
         const int URLMON_OPTION_USERAGENT = 0x10000001;
@@ -32,23 +33,29 @@ namespace AutoClicker
             InitializeComponent();
         }
 
+        //Function to find tags inside a string
         string ExtractString(string s, string tag)
         {
-            // You should check for errors in real-world code, omitted for brevity
+            // No error checking
             var startTag = tag;
             int startIndex = s.IndexOf(startTag) + startTag.Length;
             int endIndex = s.IndexOf(tag, startIndex);
             return s.Substring(startIndex, endIndex - startIndex);
         }
 
+        //Variable for mydocuments
         String myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Create Directory if it doesn't exist
             System.IO.Directory.CreateDirectory(myDocuments + @"\DG\AutoClicker");
-
+            //Hide Javascript error and change the UserAgent to be Internet Explorer 9
             webBrowser1.ScriptErrorsSuppressed = true;
             ChangeUserAgent("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)");
+
             loadingText.BringToFront();
+
+            //Perfect size to show all of the webpage
             this.Size = new Size(336, 396);
             webBrowser1.Navigate("http://mossaband.com/AutoClicker/login.html");
 
@@ -65,8 +72,8 @@ namespace AutoClicker
 
         }
 
+        //Calibrate personal Clicks Per Second for 30 seconds
         double clicks = 0;
-
         private void buttonClickFast_Click(object sender, EventArgs e)
         {
             if (Convert.ToInt32(labelSeed.Text) > 0)
@@ -81,6 +88,7 @@ namespace AutoClicker
 
         private void timerSeed_Tick(object sender, EventArgs e)
         {
+            //Make sure timer doesn't go below 0, and when it des hit 0. Stop adding to coujnter
             if (Convert.ToInt32(labelSeed.Text) > 0)
             {
                 labelSeed.Text = Convert.ToString(Convert.ToInt32(labelSeed.Text) - 1);
@@ -93,11 +101,14 @@ namespace AutoClicker
                 downloadedTemp.Visible = true;
                 instructionsLabel.Visible = true;
                 timerSeed.Stop();
+                //Diving by 30 to get Clicks Per Second
                 clicksPerSecondText.Text = Convert.ToString(clicks / 30);
+                //Write that value inside Settings.txt
                 File.WriteAllText(myDocuments + @"\DG\AutoClicker\Settings.txt", clicksPerSecondText.Text);
             }
         }
 
+        //Redo Clicks Per Second
         private void refine_Click(object sender, EventArgs e)
         {
             labelSeed.Text = "30";
@@ -110,21 +121,27 @@ namespace AutoClicker
             labelSeed.Visible = true;
         }
 
+        //Start Autoclicker
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (buttonSeed.Visible == false)
             {
+                //Start once ` is pressed
                 if (e.KeyCode == Keys.Oemtilde)
                 {
+                    //Check the current temperature
+                    //If it's less than 50 degrees (COLD), click slower.
                     if (Convert.ToDecimal(downloadedTemp.Text) <= 50)
                     {
+                        //The ClicksPerSecond is saved as a decimal.
+                        //Converting it to string
                         string autoclickInterval = Convert.ToString((1100 / Convert.ToDecimal(clicksPerSecondText.Text)));
                         autoclickInterval = autoclickInterval.Substring(0, 4);
                         timerAutoClick.Interval = Convert.ToInt32(autoclickInterval);
                     }
+                        //If it's more than 50 degrees (HOT).
                     else
                     {
-
                         string autoclickInterval = Convert.ToString((1000 / Convert.ToDecimal(clicksPerSecondText.Text)));
                         autoclickInterval = autoclickInterval.Substring(0, 4);
                         timerAutoClick.Interval = Convert.ToInt32(autoclickInterval);
@@ -155,33 +172,34 @@ namespace AutoClicker
             loadingText.Visible = false;
             if (webBrowser1.DocumentTitle.Contains("Login"))
             {
-
+                //If this is the Login page, change the size of the form
                 this.Size = new Size(336, 462);
-                // webBrowser1.Size = new Size(320, 357);
-
-
             }
             else if (webBrowser1.DocumentTitle.Contains("Create"))
             {
+                //If this is the Create an Account page, change the size of the form
                 this.Size = new Size(336, 655);
-                // webBrowser1.Size = new Size(320, 479);
-
             }
+                //This is the page after the User Logs in
             else if (webBrowser1.DocumentText.Contains("ZZ"))
             {
+                
                 webBrowser1.Visible = false;
 
                 string html = webBrowser1.DocumentText;
-
+                //The zipcode is between two 'ZZ'
                 string zipcode = ExtractString(html, "ZZ");
+                //The countrycode is between two 'CC'
                 string countrycode = ExtractString(html, "CC");
 
+                //There's a GOTO only because the website fails to load occasionally
             GetTemp:
 
                 int retry = 0;
                 try
                 {
                     WebClient Weather = new WebClient();
+                    //Plugin the zipcode and countrycode we found on the login page
                     string htmlWeather = Weather.DownloadString("http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "," + countrycode + "&units=imperial");
                     MatchCollection temperature = Regex.Matches(htmlWeather, "\"temp\":(.{5})", RegexOptions.Singleline);
 
@@ -193,6 +211,7 @@ namespace AutoClicker
                     downloadedTemp.Text = temp.Remove(0, 7);
                     this.Size = new Size(605, 403);
 
+                    //Create Settings.txt if it doesn't exist
                     if (!File.Exists(myDocuments + @"\DG\AutoClicker\Settings.txt"))
                     {
                         FileStream createSettingsTXT = new FileStream(myDocuments + @"\DG\AutoClicker\Settings.txt", FileMode.CreateNew);
@@ -200,6 +219,7 @@ namespace AutoClicker
                         buttonSeed.Visible = true;
                         labelSeed.Visible = true;
                     }
+                        //If Settings.txt already exists, that means they went ahead and calibrated their clicks per second already
                     else
                     {
                         button1.Visible = true;
@@ -211,6 +231,7 @@ namespace AutoClicker
                 }
                 catch (Exception ex)
                 {
+                    //Attempt to get the temperature 3 times before GTFO
                     if (retry <= 3)
                     {
                         goto GetTemp;
